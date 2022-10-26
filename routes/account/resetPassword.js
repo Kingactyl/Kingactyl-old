35e955499b9079f5c5567ed23f6044ea887e5f35
Yeahxd69 <115886671+Yeahxd69@users.noreply.github.com>
2022-10-26 23:12:51 +0530

    Delete routes directory

--- routes/account/resetPassword.js
+++
@@ -1,5 +0,0 @@
-module.exports.load = async function(app, ejs, db) {
-    app.get("/reset/password", async (req, res) => {
-        return res.send("Coming Soon.")
-    }
-    )}
--- routes/account/unlinkDiscord.js
+++
@@ -1,5 +0,0 @@
-module.exports.load = async function(app, ejs, db) {
-    app.get("/accounts/unlink_discord", async (req, res) => {
-        return res.send("This feature has not been completed yet.")
-    })
-}
